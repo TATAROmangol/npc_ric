abf6f8ab34d094a49f10b9366d5d559639367da1
Zagir <snuuymoou@gmail.com>
2025-04-22 23:33:29 +0500

    docx generator function

---
+++ generator/python/services/docx_service.py
@@ -0,0 +1,35 @@
+from db.models import Template, GeneratedDocument
+from db.database import SessionLocal
+from docxtpl import DocxTemplate
+import io
+import json
+
+
+class DocxGeneratorService:
+    @staticmethod
+    def generate_docx(template_name: str,
+                      data: dict,
+                      save_to_db: bool = False):
+        db = SessionLocal()
+        try:
+            template = db.query(Template).filter_by(name=template_name).first()
+            if not template:
+                return None
+            doc = DocxTemplate(io.BytesIO(template.content))
+            doc.render(data)
+            buffer = io.BytesIO()
+            doc.save(buffer)
+            file_content = buffer.getvalue()
+
+            if save_to_db:
+                new_doc = GeneratedDocument(
+                    template_id=template.id,
+                    file_content=file_content,
+                    metadata_json=json.dumps(data)
+                )
+                db.add(new_doc)
+                db.commit()
+
+            return file_content
+        finally:
+            db.close()
