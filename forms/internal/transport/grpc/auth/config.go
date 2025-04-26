d3eb45ea676d95167629e51fb0b6678dcb39e33f
TATAROmangol <deadpoolmail.ru@gmail.com>
2025-04-27 02:00:18 +0500

    refactoring cfg auth in forms

--- forms/internal/transport/grpc/auth/config.go
+++ forms/internal/transport/grpc/auth/config.go
@@ -1,10 +1,12 @@
 package admin
 
+import "fmt"
+
 type Config struct {
 	Port int `env:"FORMS_AUTH_GRPC_PORT"`
 	Host string `env:"FORMS_AUTH_GRPC_HOST"`
 }
 
 func (c *Config) Addr() string {
-	return c.Host + ":" + string(c.Port)
+	return fmt.Sprintf("%v:%v", c.Host, c.Port)
 }
