e654606dd6115d8b50965b53e834bb549ece91b7
Zagir <snuuymoou@gmail.com>
2025-05-01 13:32:06 +0500

    python server.py

---
+++ generator/python/server.py
@@ -0,0 +1,63 @@
+from concurrent import futures
+import grpc
+from db.database import SessionLocal
+from db.models import Template
+from generated import docx_generator_pb2, docx_generator_pb2_grpc
+from services.docx_service import DocxGeneratorService
+
+
+class DocxGeneratorServicer(docx_generator_pb2_grpc.DocxGeneratorServicer):
+    def UploadTemplate(self, request, context):
+        db = SessionLocal()
+        try:
+            if db.query(Template).filter_by(name=request.name).first():
+                context.set_code(grpc.StatusCode.ALREADY_EXISTS)
+                return docx_generator_pb2.Response(status="Template exists")
+
+            db.add(Template(
+                name=request.name,
+                content=request.docx_content
+            ))
+            db.commit()
+            return docx_generator_pb2.Response(status="OK")
+        except Exception as e:
+            context.set_code(grpc.StatusCode.INTERNAL)
+            return docx_generator_pb2.Response(status=f"Error: {str(e)}")
+        finally:
+            db.close()
+
+    def GenerateDocx(self, request, context):
+        try:
+            # Преобразование TableData в dict для docxtpl
+            data = {
+                col: [row.values[i] for row in request.table.rows]
+                for i, col in enumerate(request.table.columns)
+            }
+
+            content = DocxGeneratorService.generate_docx(
+                template_name=request.template_name,
+                data=data
+            )
+            if not content:
+                context.set_code(grpc.StatusCode.NOT_FOUND)
+                return docx_generator_pb2.GenerateResponse()
+
+            return docx_generator_pb2.GenerateResponse(result_docx=content)
+        except Exception:
+            context.set_code(grpc.StatusCode.INTERNAL)
+            return docx_generator_pb2.GenerateResponse()
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    docx_generator_pb2_grpc.add_DocxGeneratorServicer_to_server(
+        DocxGeneratorServicer(), server
+    )
+    server.add_insecure_port("[::]:50051")
+    server.start()
+    print("Server running on port 50051")
+    server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    serve()
