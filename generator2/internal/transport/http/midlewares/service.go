ba57474c5eac593aebc7eab82adfc0ae34905721
TATAROmangol <deadpoolmail.ru@gmail.com>
2025-06-18 03:13:59 +0500

    complete midlewares

--- generator2/internal/transport/http/midlewares/service.go
+++ generator2/internal/transport/http/midlewares/service.go
@@ -20,6 +20,20 @@ func (m *Midleware) InitLoggerCtx() mux.MiddlewareFunc{
 func (m *Midleware) CheckAuth(cookieName string) mux.MiddlewareFunc{
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			cookie, err := r.Cookie("admin_token")
+			if err != nil || cookie == nil {	
+				logger.GetFromCtx(r.Context()).ErrorContext(r.Context(), "cookie not found", err)
+				http.Error(w, "cookie not found", http.StatusUnauthorized)
+				return
+			}
+
+			ok, err := m.v.Verify(r.Context(), cookie.Value)
+			if err != nil || !ok {
+				logger.GetFromCtx(r.Context()).ErrorContext(r.Context(), "invalid token", err)
+				http.Error(w, "invalid token", http.StatusUnauthorized)
+				return
+			}
+
 			next.ServeHTTP(w, r)
 		})
 	}
