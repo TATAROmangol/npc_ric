e52871f3704d0628bfcc046cfc8e23b1ea9bd2f1
TATAROmangol <deadpoolmail.ru@gmail.com>
2025-06-18 15:03:04 +0500

    add mongo loyault

---
+++ generator2/pkg/mongodb/config.go
@@ -0,0 +1,17 @@
+package mongodb
+
+import "fmt"
+
+type Config struct {
+	Host           string `env:"MONGO_HOST"`
+	Port           string `env:"MONGO_PORT"`
+	User           string `env:"MONGO_USER"`
+	Password       string `env:"MONGO_PASSWORD"`
+	DBName         string `env:"MONGO_DB_NAME"`
+	CollectionName string `env:"MONGO_COLLECTION_NAME"`
+	SSL            string `env:"MONGO_SSL"`
+}
+
+func (c *Config) Addr() string {
+	return fmt.Sprintf("mongodb://%v:%v@%v:%v/%v?ssl=%v", c.User, c.Password, c.Host, c.Port, c.DBName, c.SSL)
+}
